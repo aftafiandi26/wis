1327b933b25c07ba3c642af3319ee2f1f35d3a3e
dede aftafiandi <hattpri.yhukimura@gmail.com>
2024-07-24 14:26:12 +0700

    template_admin #1

---
+++ resources/views/layouts/template_admin/style.blade.php
@@ -0,0 +1,33 @@
+<link
+      rel="icon"
+      href="{{ asset('template/administrator/assets/img/kaiadmin/favicon.ico') }}"
+      type="image/x-icon"
+    />
+
+    <!-- Fonts and icons -->
+    <script src="{{ asset('template/administrator/assets/js/plugin/webfont/webfont.min.js') }}"></script>
+    <script>
+      WebFont.load({
+        google: { families: ["Public Sans:300,400,500,600,700"] },
+        custom: {
+          families: [
+            "Font Awesome 5 Solid",
+            "Font Awesome 5 Regular",
+            "Font Awesome 5 Brands",
+            "simple-line-icons",
+          ],
+          urls: ["{{ asset('template/administrator/assets/css/fonts.min.css') }}"],
+        },
+        active: function () {
+          sessionStorage.fonts = true;
+        },
+      });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="{{ asset('template/administrator/assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template/administrator/assets/css/plugins.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template/administrator/assets/css/kaiadmin.min.css') }}" />
+
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link rel="stylesheet" href="{{ asset('template/administrator/assets/css/demo.css') }}" />
